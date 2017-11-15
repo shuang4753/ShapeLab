31a87a6266cfb6a721f00a82d7e166b4de0a01d3
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-11-15 08:58:33 -0500

    FINNISHED

--- ClassWork/src/shapes/Kite.java
+++ ClassWork/src/shapes/Kite.java
@@ -6,12 +6,12 @@ package shapes;
  */
 public class Kite implements Shape {
 
-	private double diagonal1;
-	private double diagonal2;
+	private int diagonal1;
+	private int diagonal2;
 	private int side1;
 	private int side2;
 
-	public Kite(double diagonal1, double diagonal2, int side1, int side2) {
+	public Kite(int diagonal1, int diagonal2, int side1, int side2) {
 		this.diagonal1 = diagonal1;
 		this.diagonal2 = diagonal2;
 		this.side1 = side1;
@@ -32,7 +32,9 @@ public class Kite implements Shape {
 
 
 	public String toString() {
-		return "Kite Diagonal 1: " + diagonal1 + " Diagonal 2: " + diagonal2 + "Side 1" + side1 + "Side 2" + side2 + "Area: " + this.calculateArea() + " Perimeter: "
-				+ this.calculatePerimeter();
+		return "Kite Diagonal 1: " + diagonal1 + "  Diagonal 2: " + diagonal2
+				+ "  Side 1: " + side1 + "  Side 2: " + side2
+				+ "  Area: " + this.calculateArea() 
+				+ "  Perimeter: " + this.calculatePerimeter();
 	}
 }
