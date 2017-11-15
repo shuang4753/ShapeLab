91682e97be5afe3853915a78db608878bf1135ef
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-11-15 09:14:00 -0500

    hepatgon comments

--- ClassWork/src/shapes/Heptagon.java
+++ ClassWork/src/shapes/Heptagon.java
@@ -5,28 +5,32 @@ package shapes;
  *
  */
 public class Heptagon implements Shape {
-
+	/**
+	 * @param side
+	 */
 	private int side;
 	private double pi = 3.14;
-
+/**
+ * Constructor for heptagon
+ */
 	public Heptagon(int side) {
 		this.side = side;
 		
 	}
 
-
+//Calculates area for heptagon = (7/4)a^2(cot(pi/7)), Also adds to total area calculated
 	public double calculateArea() {
 		
 		return (1.75)*(side*side)*(Math.cos(pi/7)/Math.sin(pi/7));
 	}
 
-
+// perimeter is the 7 sides added up, Also adds to total perimeter calculated
 	public double calculatePerimeter() {
 		
 		return 7*side;
 	}
 
-
+//prints to console
 	public String toString() {
 		return "Heptagon Side: " + side + "   Area: " +
 				this.calculateArea() + " Perimeter: "
